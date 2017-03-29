1e7cb57a1610d73ed12b056896c322476f9be093
Yenifer Prajapati <yen.a@me.com>
2017-03-29 10:19:42 -0400

    Update seed data for videos linked to therapies
    Use an array of elements to include seed data for videos table.

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,6 +15,31 @@ Therapy.create([{ name: 'Speech Emission' },
 
 Video.create([{
                therapy: Therapy.first,
-               name: 'vowels',
-               url: 'https://www.youtube.com/watch?v=BGa3AqeqRy0&list=PLjxkFbiVAu_OLWF7tv-eY9ehsXLyX2D4y'
-             }])
+               name: 'Repeat and Identify',
+               url: 'https://www.youtube.com/watch?v=BLuZdiX7Wrg'
+             },
+              {
+                therapy: Therapy.first,
+                name: 'Pronunciation',
+                url: 'https://www.youtube.com/watch?v=ky2hngeUcUg'
+              },
+              {
+                therapy: Therapy.find(2),
+                name: 'Articulation Game',
+                url: 'https://www.youtube.com/watch?v=XhuU3c5QSso'
+              },
+              {
+                therapy: Therapy.find(2),
+                name: 'S and Z',
+                url: 'https://www.youtube.com/watch?v=kTAtVWsvhNA'
+              },
+              {
+                therapy: Therapy.last,
+                name: 'Escape of Air',
+                url: 'https://www.youtube.com/watch?v=k1juKhiqReU'
+              },
+              {
+                therapy: Therapy.last,
+                name: 'Practice S',
+                url: 'https://www.youtube.com/watch?v=gHVtHEc4PU4'
+              }])
